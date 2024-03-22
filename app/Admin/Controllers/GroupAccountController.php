827c7176793e07d00dcc19bc214786a08793cb90
BANADDA MUBARAKA <83466862+BANADDA@users.noreply.github.com>
2024-03-22 09:59:42 +0300

    allow duplicates

--- app/Admin/Controllers/GroupAccountController.php
+++ app/Admin/Controllers/GroupAccountController.php
@@ -28,45 +28,83 @@ class GroupAccountController extends AdminController
      * @return Grid
      */
     protected function grid()
-    {
-        $grid = new Grid(new User());
-        $u = Admin::user();
-        $admin = Admin::user();
-        $adminId = $admin->id;
-        if (!$admin->isRole('admin')) {
-    
-            $orgAllocation = OrgAllocation::where('user_id', $adminId)->first();
-            if ($orgAllocation) {
-                $orgId = $orgAllocation->vsla_organisation_id;
-                // die(print_r($orgId));
-                $organizationAssignments = VslaOrganisationSacco::where('vsla_organisation_id', $orgId)->get();
+{
+    $grid = new Grid(new User());
+    $u = Admin::user();
+    $admin = Admin::user();
+    $adminId = $admin->id;
+    if (!$admin->isRole('admin')) {
 
-                // Extracting Sacco IDs from the assignments
-                $saccoIds = $organizationAssignments->pluck('sacco_id')->toArray();
-                // die(print_r($saccoIds));
+        $orgAllocation = OrgAllocation::where('user_id', $adminId)->first();
+        if ($orgAllocation) {
+            $orgId = $orgAllocation->vsla_organisation_id;
+            $organizationAssignments = VslaOrganisationSacco::where('vsla_organisation_id', $orgId)->get();
 
-                $grid->model()->whereIn('sacco_id', $saccoIds);
-                $grid->disableCreateButton();
-                $grid->actions(function (Grid\Displayers\Actions $actions) {
-                    $actions->disableDelete();
-                });
-                $grid->disableFilter();
-            }
+            // Extracting Sacco IDs from the assignments
+            $saccoIds = $organizationAssignments->pluck('sacco_id')->toArray();
+
+            $grid->model()->whereIn('sacco_id', $saccoIds);
+            $grid->disableCreateButton();
+            $grid->actions(function (Grid\Displayers\Actions $actions) {
+                $actions->disableDelete();
+            });
+            $grid->disableFilter();
         }
-        $grid->model()->where('user_type', 'Admin');
-    
-        $grid->disableBatchActions();
-        $grid->quickSearch('first_name', 'last_name', 'email', 'phone_number')->placeholder('Search by name, email or phone number');
-    
-        $grid->column('name', __('Account Name'))->display(function () {
-            return $this->first_name . ' ' . $this->last_name;
-        });
-        $grid->column('phone_number', __('Contact'));
-        // $grid->column('email', __('Email'))->sortable();
-        $grid->column('created_at', __('Date Created'))->sortable();
-    
-        return $grid;
-    }    
+    }
+    $grid->model()->where('user_type', 'Admin')->groupBy('sacco_id'); // Group by sacco_id to allow duplicates
+
+    $grid->disableBatchActions();
+    $grid->quickSearch('first_name', 'last_name', 'email', 'phone_number')->placeholder('Search by name, email or phone number');
+
+    $grid->column('name', __('Account Name'))->display(function () {
+        return $this->first_name . ' ' . $this->last_name;
+    });
+    $grid->column('phone_number', __('Contact'));
+    $grid->column('created_at', __('Date Created'))->sortable();
+
+    return $grid;
+}
+
+    // protected function grid()
+    // {
+    //     $grid = new Grid(new User());
+    //     $u = Admin::user();
+    //     $admin = Admin::user();
+    //     $adminId = $admin->id;
+    //     if (!$admin->isRole('admin')) {
+
+    //         $orgAllocation = OrgAllocation::where('user_id', $adminId)->first();
+    //         if ($orgAllocation) {
+    //             $orgId = $orgAllocation->vsla_organisation_id;
+    //             // die(print_r($orgId));
+    //             $organizationAssignments = VslaOrganisationSacco::where('vsla_organisation_id', $orgId)->get();
+
+    //             // Extracting Sacco IDs from the assignments
+    //             $saccoIds = $organizationAssignments->pluck('sacco_id')->toArray();
+    //             // die(print_r($saccoIds));
+
+    //             $grid->model()->whereIn('sacco_id', $saccoIds);
+    //             $grid->disableCreateButton();
+    //             $grid->actions(function (Grid\Displayers\Actions $actions) {
+    //                 $actions->disableDelete();
+    //             });
+    //             $grid->disableFilter();
+    //         }
+    //     }
+    //     $grid->model()->where('user_type', 'Admin');
+
+    //     $grid->disableBatchActions();
+    //     $grid->quickSearch('first_name', 'last_name', 'email', 'phone_number')->placeholder('Search by name, email or phone number');
+
+    //     $grid->column('name', __('Account Name'))->display(function () {
+    //         return $this->first_name . ' ' . $this->last_name;
+    //     });
+    //     $grid->column('phone_number', __('Contact'));
+    //     // $grid->column('email', __('Email'))->sortable();
+    //     $grid->column('created_at', __('Date Created'))->sortable();
+
+    //     return $grid;
+    // }
 
     /**
      * Make a show builder.
