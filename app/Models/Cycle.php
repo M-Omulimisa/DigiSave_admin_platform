ccba65c304c4f90bb56e7c67258a07623e5630be
BANADDA MUBARAKA <83466862+BANADDA@users.noreply.github.com>
2024-06-20 17:26:58 +0300

    modified:   app/Models/Cycle.php

--- app/Models/Cycle.php
+++ app/Models/Cycle.php
@@ -8,16 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Cycle extends Model
 {
     use HasFactory;
-    //boot
-    public static function boot()
+
+    // Define the relationship with transactions
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class);
+    }
+
+    // Boot method to apply the cascading delete
+    protected static function boot()
     {
         parent::boot();
-        self::deleting(function ($m) {
-            Transaction::where([
-                'cycle_id' => $m->id
-            ])->delete();
-            //throw new \Exception("Cannot delete Cycle");
+
+        static::deleting(function ($cycle) {
+            // Delete all related transactions
+            $cycle->transactions()->delete();
         });
+
         self::creating(function ($m) {
             if ($m->status == 'Active') {
                 $old = Cycle::where('sacco_id', $m->sacco_id)->where('status', 'Active')->first();
@@ -32,16 +39,10 @@ class Cycle extends Model
             //only if created by status is active for a sacco
             if ($m->status == 'Active') {
                 $old = Cycle::where('sacco_id', $m->sacco_id)->where('status', 'Active')->first();
-                if ($old != null)
-                    if ($old->id != $m->id) {
-                        throw new \Exception("Sacco already has an active cycle");
-                    }
+                if ($old != null && $old->id != $m->id) {
+                    throw new \Exception("Sacco already has an active cycle");
+                }
             }
-        });
-
-        self::creating(function ($m) {
-
-            return $m;
         });
     }
 }
