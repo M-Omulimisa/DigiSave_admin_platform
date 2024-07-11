629e49736ecf51120671dc0084bde5609149a76f
BANADDA MUBARAKA <83466862+BANADDA@users.noreply.github.com>
2024-07-11 07:09:47 +0300

    modified:   app/Admin/Controllers/HomeController.php

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -201,7 +201,7 @@ class HomeController extends Controller
                 ->count();
 
             $pwdTotalLoanBalance = Transaction::whereIn('sacco_id', $saccoIds)->where('type', 'LOAN')
-                ->whereIn('user_id', $pwdUserIds)
+                ->whereIn('source_user_id', $pwdUserIds)
                 ->sum('balance');
 
             $totalLoanAmount = Transaction::whereIn('sacco_id', $saccoIds)->whereIn('user_id', $filteredUsers->pluck('id'))
@@ -312,10 +312,10 @@ class HomeController extends Controller
                 ->where('users.sex', 'Male')
                 ->sum('transactions.amount');
 
-            $pwdTotalLoanBalance = Transaction::join('users', 'transactions.source_user_id', '=', 'users.id')
-            ->where('transactions.type', 'LOAN')
-            ->where('pwd', 'yes')
-            ->sum('transactions.amount');
+            // $pwdTotalLoanBalance = Transaction::join('users', 'transactions.source_user_id', '=', 'users.id')
+            // ->where('transactions.type', 'LOAN')
+            // ->where('pwd', 'yes')
+            // ->sum('transactions.amount');
 
             // Count loans disbursed to youths
             $loansDisbursedToYouths = Transaction::whereIn('source_user_id', $youthIds)
@@ -338,9 +338,9 @@ class HomeController extends Controller
                 ->whereIn('source_user_id', $pwdUserIds)
                 ->count();
 
-            // $pwdTotalLoanBalance = Transaction::where('type', 'LOAN')
-            //     ->whereIn('source_user_id', $pwdUserIds)
-            //     ->sum('balance');
+            $pwdTotalLoanBalance = Transaction::where('type', 'LOAN')
+                ->whereIn('user_id', $pwdUserIds)
+                ->sum('balance');
 
             $transactions = Transaction::all();
             $monthYearList = [];
