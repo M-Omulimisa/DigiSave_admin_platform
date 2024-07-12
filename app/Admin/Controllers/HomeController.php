07f6869a11c4dad93641af24de56948b3a87eff2
BANADDA MUBARAKA <83466862+BANADDA@users.noreply.github.com>
2024-07-12 13:27:25 +0300

    modified:   app/Admin/Controllers/HomeController.php

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -469,7 +469,7 @@ class HomeController extends Controller
             $maleTotalBalance = Transaction::join('users', 'transactions.user_id', '=', 'users.id')
                 ->whereIn('transactions.sacco_id', $saccoIds)
                 ->where('users.sex', 'Male')
-                ->where('users.sacco_join_status', 'Approved')
+                // ->where('users.sacco_join_status', 'Approved')
                 ->where('users.user_type', '!=', 'admin')
                 ->where('transactions.type', 'SHARE')
                 ->sum('transactions.balance');
@@ -477,7 +477,7 @@ class HomeController extends Controller
                 $femaleTotalBalance = Transaction::join('users', 'transactions.user_id', '=', 'users.id')
                 ->whereIn('transactions.sacco_id', $saccoIds)
                 ->where('users.sex', 'Female')
-                ->where('users.sacco_join_status', 'Approved')
+                // ->where('users.sacco_join_status', 'Approved')
                 ->where('users.user_type', '!=', 'admin')
                 ->where('transactions.type', 'SHARE')
                 ->sum('transactions.balance');
@@ -651,7 +651,7 @@ class HomeController extends Controller
                 ->whereIn('transactions.source_user_id', $filteredUsersIds)
                 ->where('transactions.type', 'SHARE')
                 ->where('users.sex', 'Male')
-                ->where('users.sacco_join_status', 'Approved')
+                // ->where('users.sacco_join_status', 'Approved')
                 ->where('users.user_type', '!=', 'admin')
                 ->sum('transactions.balance');
 
@@ -659,7 +659,7 @@ class HomeController extends Controller
                 ->whereIn('transactions.source_user_id', $filteredUsersIds)
                 ->where('transactions.type', 'SHARE')
                 ->where('users.sex', 'Female')
-                ->where('users.sacco_join_status', 'Approved')
+                // ->where('users.sacco_join_status', 'Approved')
                 ->where('users.user_type', '!=', 'admin')
                 ->sum('transactions.balance');
 
