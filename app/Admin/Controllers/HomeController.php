0328aac5155b32b097d91c370e26ccb8e27a8e50
BANADDA MUBARAKA <83466862+BANADDA@users.noreply.github.com>
2024-08-06 09:33:45 +0300

    modified:   app/Admin/Controllers/HomeController.php

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -132,7 +132,7 @@ class HomeController extends Controller
     $totalBalance = User::join('transactions as t', 'users.id', '=', 't.source_user_id')
         ->join('saccos as s', 'users.sacco_id', '=', 's.id')
         ->whereIn('users.id', $userIds)  // Use the extracted user IDs
-        ->where('users.sex', 'Female')
+        // ->where('users.sex', 'Female')
         ->whereNotIn('users.sacco_id', $deletedOrInactiveSaccoIds)
         ->where('t.type', $type)  // Use the specified transaction type
         ->where(function ($query) {
