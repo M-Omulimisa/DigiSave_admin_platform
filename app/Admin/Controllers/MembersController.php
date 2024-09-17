759fa9e7154cc0cd02c79fa1ed1439387801c630
BANADDA MUBARAKA <83466862+BANADDA@users.noreply.github.com>
2024-09-17 11:11:54 +0300

    modified:   app/Admin/Controllers/MembersController.php

--- app/Admin/Controllers/MembersController.php
+++ app/Admin/Controllers/MembersController.php
@@ -29,97 +29,107 @@ class MembersController extends AdminController
      * @return Grid
      */
     protected function grid()
-    {
-        $grid = new Grid(new User());
+{
+    $grid = new Grid(new User());
 
-        $admin = Admin::user();
-        $adminId = $admin->id;
+    $admin = Admin::user();
+    $adminId = $admin->id;
 
-        // Default sort order
-        $sortOrder = request()->get('_sort', 'desc');
-        if (!in_array($sortOrder, ['asc', 'desc'])) {
-            $sortOrder = 'desc';
-        }
+    // Default sort order
+    $sortOrder = request()->get('_sort', 'desc');
+    if (!in_array($sortOrder, ['asc', 'desc'])) {
+        $sortOrder = 'desc';
+    }
 
-        // Restrict access for non-admin users
-        if (!$admin->isRole('admin')) {
-            $orgAllocation = OrgAllocation::where('user_id', $adminId)->first();
-            if ($orgAllocation) {
-                $orgId = $orgAllocation->vsla_organisation_id;
-                $organizationAssignments = VslaOrganisationSacco::where('vsla_organisation_id', $orgId)->get();
-                $saccoIds = $organizationAssignments->pluck('sacco_id')->toArray();
-                $grid->model()
-                    ->whereIn('sacco_id', $saccoIds)
-                    ->where(function ($query) {
-                        $query->whereNull('user_type')
-                              ->orWhere('user_type', '!=', 'Admin');
-                    })
-                    ->orderBy('created_at', $sortOrder);
-                $grid->disableCreateButton();
-                $grid->actions(function (Grid\Displayers\Actions $actions) {
-                    $actions->disableDelete();
-                });
-            }
-        } else {
+    // Restrict access for non-admin users
+    if (!$admin->isRole('admin')) {
+        $orgAllocation = OrgAllocation::where('user_id', $adminId)->first();
+        if ($orgAllocation) {
+            $orgId = $orgAllocation->vsla_organisation_id;
+            $organizationAssignments = VslaOrganisationSacco::where('vsla_organisation_id', $orgId)->get();
+            $saccoIds = $organizationAssignments->pluck('sacco_id')->toArray();
             $grid->model()
+                ->whereIn('sacco_id', $saccoIds)
                 ->where(function ($query) {
                     $query->whereNull('user_type')
-                          ->orWhere('user_type', '!=', 'Admin');
+                        ->orWhere('user_type', '!=', 'Admin');
                 })
                 ->orderBy('created_at', $sortOrder);
+            $grid->disableCreateButton();
+            $grid->actions(function (Grid\Displayers\Actions $actions) {
+                $actions->disableDelete();
+            });
         }
-
-        $grid->disableBatchActions();
-        $grid->quickSearch('first_name', 'last_name', 'email', 'phone_number')->placeholder('Search by name, email or phone number');
-
-        $grid->column('first_name', __('First Name'))->sortable()->display(function ($firstName) {
-            return ucwords(strtolower($firstName));
-        });
-        $grid->column('last_name', __('Last Name'))->sortable()->display(function ($lastName) {
-            return ucwords(strtolower($lastName));
-        });
-        $grid->column('sex', __('Gender'))->sortable();
-        $grid->column('phone_number', __('Phone Number'))->display(function ($phoneNumber) {
-            // Check if the phone number is valid (Ugandan phone number format example)
-            $isValidPhoneNumber = preg_match('/^(\+256|0)?[3-9][0-9]{8}$/', $phoneNumber);
-            return $isValidPhoneNumber ? $phoneNumber : '';
-        });
-        $grid->column('sacco.name', __('Group Name'))->sortable();
-        $grid->column('created_at', __('Date Joined'))->sortable()->display(function ($date) {
-            return date('d M Y', strtotime($date));
-        });
-        // $grid->column('district', __('District'))->sortable();
-        // $grid->column('subcounty', __('Subcounty'))->sortable();
-        // $grid->column('parish', __('Parish'))->sortable();
-        // $grid->column('village', __('Village'))->sortable();
-
-        // Adding search filters
-        $grid->filter(function ($filter) {
-            $filter->disableIdFilter();
-
-            $filter->like('first_name', 'First Name');
-            $filter->like('last_name', 'Last Name');
-            $filter->like('email', 'Email');
-            $filter->like('phone_number', 'Phone Number');
-        });
-
-        // Adding custom dropdown for sorting
-        $grid->tools(function ($tools) {
-            $tools->append('
-                <div class="btn-group pull-right" style="margin-right: 10px; margin-left: 10px;">
-                    <button type="button" class="btn btn-sm btn-default dropdown-toggle" data-toggle="dropdown">
-                        Sort by Created Date <span class="caret"></span>
-                    </button>
-                    <ul class="dropdown-menu" role="menu">
-                        <li><a href="'.url()->current().'?_sort=asc">Ascending</a></li>
-                        <li><a href="'.url()->current().'?_sort=desc">Descending</a></li>
-                    </ul>
-                </div>
-            ');
-        });
-
-        return $grid;
+    } else {
+        $grid->model()
+            ->where(function ($query) {
+                $query->whereNull('user_type')
+                    ->orWhere('user_type', '!=', 'Admin');
+            })
+            ->orderBy('created_at', $sortOrder);
     }
+
+    $grid->disableBatchActions();
+    $grid->quickSearch('first_name', 'last_name', 'email', 'phone_number')->placeholder('Search by name, email or phone number');
+
+    $grid->column('first_name', __('First Name'))->sortable()->display(function ($firstName) {
+        return ucwords(strtolower($firstName));
+    });
+    $grid->column('last_name', __('Last Name'))->sortable()->display(function ($lastName) {
+        return ucwords(strtolower($lastName));
+    });
+    $grid->column('sex', __('Gender'))->sortable();
+    $grid->column('phone_number', __('Phone Number'))->display(function ($phoneNumber) {
+        // Check if the phone number is valid (Ugandan phone number format example)
+        $isValidPhoneNumber = preg_match('/^(\+256|0)?[3-9][0-9]{8}$/', $phoneNumber);
+        return $isValidPhoneNumber ? $phoneNumber : '';
+    });
+    $grid->column('sacco.name', __('Group Name'))->sortable();
+    $grid->column('created_at', __('Date Joined'))->sortable()->display(function ($date) {
+        return date('d M Y', strtotime($date));
+    });
+
+    // Adding search filters
+    $grid->filter(function ($filter) {
+        $filter->disableIdFilter();
+
+        $filter->like('first_name', 'First Name');
+        $filter->like('last_name', 'Last Name');
+        $filter->like('email', 'Email');
+        $filter->like('phone_number', 'Phone Number');
+    });
+
+    // Adding a custom button for filtering members without a gender
+    $grid->tools(function ($tools) {
+        // Add a custom button to filter members with no gender
+        $tools->append('
+            <div class="btn-group pull-right" style="margin-right: 10px;">
+                <a href="'.url()->current().'?_gender_filter=none" class="btn btn-sm btn-default">Members Without Gender</a>
+            </div>
+        ');
+
+        // Add a dropdown for sorting
+        $tools->append('
+            <div class="btn-group pull-right" style="margin-right: 10px; margin-left: 10px;">
+                <button type="button" class="btn btn-sm btn-default dropdown-toggle" data-toggle="dropdown">
+                    Sort by Created Date <span class="caret"></span>
+                </button>
+                <ul class="dropdown-menu" role="menu">
+                    <li><a href="'.url()->current().'?_sort=asc">Ascending</a></li>
+                    <li><a href="'.url()->current().'?_sort=desc">Descending</a></li>
+                </ul>
+            </div>
+        ');
+    });
+
+    // Check if the custom filter button was clicked
+    if (request()->get('_gender_filter') === 'none') {
+        $grid->model()->whereNull('sex');
+    }
+
+    return $grid;
+}
+
 
     /**
      * Make a show builder.
