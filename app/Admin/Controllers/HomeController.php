fcbb76a9e64ef5cd0e72fc9261004ab90a4d91c9
BANADDA MUBARAKA <83466862+BANADDA@users.noreply.github.com>
2024-10-18 06:26:36 +0300

    modified:   app/Admin/Controllers/HomeController.php

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -542,7 +542,7 @@ private function formatCurrency($amount)
             $transactions = Transaction::join('users', 'transactions.source_user_id', '=', 'users.id')
             ->join('saccos', 'users.sacco_id', '=', 'saccos.id')
             ->whereIn('saccos.id', $saccoIds) // Ensure this checks 'saccos.id' rather than 'sacco_id'
-            // ->whereNotIn('users.sacco_id', $deletedOrInactiveSaccoIds)
+            ->whereNotIn('users.sacco_id', $deletedOrInactiveSaccoIds)
             ->where('transactions.type', 'SHARE') // Filter for 'SHARE' type transactions
             ->where(function ($query) {
                 $query->whereNull('users.user_type')
@@ -793,7 +793,7 @@ private function formatCurrency($amount)
 
             $transactions = Transaction::join('users', 'transactions.source_user_id', '=', 'users.id')
             ->join('saccos', 'users.sacco_id', '=', 'saccos.id')
-            // ->whereNotIn('users.sacco_id', $deletedOrInactiveSaccoIds)
+            ->whereNotIn('users.sacco_id', $deletedOrInactiveSaccoIds)
             ->where('transactions.type', 'SHARE') // Filter for 'SHARE' type transactions
             ->where(function ($query) {
                 $query->whereNull('users.user_type')
