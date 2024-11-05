fe9751e33294b311fc656743222af6fa619ab587
BANADDA MUBARAKA <83466862+BANADDA@users.noreply.github.com>
2024-11-05 06:51:44 +0300

    modified:   app/Admin/Controllers/HomeController.php

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -560,7 +560,14 @@ private function formatCurrency($amount)
             });
 
             // Output the October transactions
-            dd($octoberTransactions);
+            dd($octoberTransactions->map(function ($transaction) {
+                return [
+                    'id' => $transaction->id,
+                    'amount' => $transaction->amount,
+                    'user_id' => $transaction->user_id,
+                    'source_user_id' => $transaction->source_user_id,
+                ];
+            }));
 
             foreach ($transactions as $transaction) {
                 $monthYear = Carbon::parse($transaction->created_at)->format('F Y');
@@ -820,7 +827,14 @@ private function formatCurrency($amount)
             });
 
             // Output the October transactions
-            dd($octoberTransactions);
+            dd($octoberTransactions->map(function ($transaction) {
+                return [
+                    'id' => $transaction->id,
+                    'amount' => $transaction->amount,
+                    'user_id' => $transaction->user_id,
+                    'source_user_id' => $transaction->source_user_id,
+                ];
+            }));
 
             foreach ($transactions as $transaction) {
                 $monthYear = Carbon::parse($transaction->created_at)->format('F Y');
