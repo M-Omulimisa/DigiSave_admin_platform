7bc00737670ae84536a01b70ffc6b37599a429cf
BANADDA MUBARAKA <83466862+BANADDA@users.noreply.github.com>
2024-11-05 06:28:14 +0300

    modified:   resources/views/widgets/chart_container.blade.php

--- resources/views/widgets/chart_container.blade.php
+++ resources/views/widgets/chart_container.blade.php
@@ -1,11 +1,13 @@
 <div class="row">
-    <div class="col-md-6">
+    <div class="col-md-12">
         <div class="chart-container" style="padding: 20px; background-color:white; border: 1px solid #ddd; border-radius: 5px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
             @include('admin.chartjs', [$Male, $Female])
         </div>
     </div>
+</div>
 
-    <div class="col-md-6">
+<div class="row mt-4"> <!-- Add margin-top for spacing between rows -->
+    <div class="col-md-12">
         <div class="chart-container" style="padding: 20px; background-color:white; border: 1px solid #ddd; border-radius: 5px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
             @include('admin.line_chartjs', ['monthYearList' => $monthYearList, 'totalSavingsList' => $totalSavingsList])
         </div>
