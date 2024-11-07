761f039adb3097f7bcd85810c077dac4cac52e6f
BANADDA MUBARAKA <83466862+BANADDA@users.noreply.github.com>
2024-11-07 07:30:05 +0300

    modified:   resources/views/widgets/chart_container.blade.php

--- resources/views/widgets/chart_container.blade.php
+++ resources/views/widgets/chart_container.blade.php
@@ -1,11 +1,11 @@
 <div class="row">
-    <div class="col-md-6">
+    <div class="col-md-4">
         <div class="chart-container" style="padding: 20px; background-color:white; border: 1px solid #ddd; border-radius: 5px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
             @include('admin.chartjs', [$Male, $Female])
         </div>
     </div>
 
-    <div class="col-md-6">
+    <div class="col-md-8">
         <div class="chart-container" style="padding: 20px; background-color:white; border: 1px solid #ddd; border-radius: 5px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
             @include('admin.line_chartjs', ['monthYearList' => $monthYearList, 'totalSavingsList' => $totalSavingsList])
         </div>
