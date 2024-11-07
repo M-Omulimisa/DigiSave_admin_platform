30609f980e4f02c7e6ba85c0e32e49c6c6f6cfda
BANADDA MUBARAKA <83466862+BANADDA@users.noreply.github.com>
2024-11-07 07:32:13 +0300

    modified:   resources/views/widgets/chart_container.blade.php

--- resources/views/widgets/chart_container.blade.php
+++ resources/views/widgets/chart_container.blade.php
@@ -1,14 +1,12 @@
 <div class="row">
-    <!-- First Chart (Narrower) -->
-    <div class="col-md-5">
-        <div class="chart-container" style="padding: 20px; background-color: white; border: 1px solid #ddd; border-radius: 5px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
+    <div class="col-md-6">
+        <div class="chart-container" style="padding: 20px; background-color:white; border: 1px solid #ddd; border-radius: 5px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
             @include('admin.chartjs', [$Male, $Female])
         </div>
     </div>
 
-    <!-- Line Chart (Wider) -->
     <div class="col-md-6">
-        <div class="chart-container" style="padding: 20px; background-color: white; border: 1px solid #ddd; border-radius: 5px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
+        <div class="chart-container" style="padding: 20px; background-color:white; border: 1px solid #ddd; border-radius: 5px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
             @include('admin.line_chartjs', ['monthYearList' => $monthYearList, 'totalSavingsList' => $totalSavingsList])
         </div>
     </div>
