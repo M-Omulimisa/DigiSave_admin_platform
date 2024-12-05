9a32bd139d079a7fbac99117f20cc534f5ad778b
BANADDA MUBARAKA <83466862+BANADDA@users.noreply.github.com>
2024-12-05 17:34:35 +0300

    modified:   config/admin.php

--- config/admin.php
+++ config/admin.php
@@ -372,7 +372,7 @@ return [
     |
     | Whether enable default breadcrumb for every page content.
     */
-    'enable_default_breadcrumb' => true,
+    'enable_default_breadcrumb' => false,
 
     /*
     |--------------------------------------------------------------------------
